2f7bbb87f67f0ff29a0d89943edbb61d6779d3af
hullerhuller <huller.ram@gmail.com>
2021-09-27 17:54:57 +0900

    prototype vuetify＋クラス導入

---
+++ logic/indexpage.ts
@@ -0,0 +1,75 @@
+
+import { Component, Vue, Prop, Emit } from 'vue-property-decorator'
+
+interface logItem {
+    avatar: string,
+    title: string,
+    text: string,
+    comment:string
+}
+
+interface chooseItem extends logItem {
+    choosen: number
+}
+
+@Component({
+})
+export default class PostPreview extends Vue {
+
+    formError: string = ''
+    dateJi: string = ''
+    dateIt: string = ''
+
+    choosen: chooseItem[] = [{
+        avatar: '/favicon.ico',
+        title: '1',
+        text: `<font color="red">Ali Connors</font> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        comment:'yeah',
+        choosen: 1
+    }, {
+        avatar: '/favicon.ico',
+        title: '2',
+        text: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        comment:'yeah',
+        choosen: 2
+    }, {
+        avatar: '/favicon.ico',
+        title: '3?',
+        text: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        comment:'yeah',
+        choosen: 3
+    }
+
+    ]
+
+    logs: logItem[] = [{
+        avatar: '/favicon.ico',
+        title: 'Brunch this weekend?',
+        text: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        comment:'yeah',
+    }
+    ]
+
+    mounted() {
+    }
+
+    chooseCard(chooseCard: chooseItem) {
+        this.logs.push(chooseCard)
+        if(document.getElementById('scroll') !=null){
+            document.getElementById('scroll')!.scrollTo(0, 10000000);
+        }
+    }
+
+
+    next() {
+        this.formError = ''
+
+    }
+
+    backMenu() {
+
+        this.$router.push('/')
+    }
+
+
+}
